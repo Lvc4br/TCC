90a85d06e0f65e540ee092d70dbe80ecefaed144
Luca Toniolo <lucatoniolo.br@gmail.com>
2025-09-13 12:36:30 -0300

    Adicionando componentes, Pages criadas

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import './hero.css'
 
 const hero = () => {
   return (
-    <div>
-        hero
-    </div>
+    <div className="dashboard">
+      <h2>Bem-vindo, Luca</h2>
+        <div className="cards">
+          <div className="card">💰 Saldo Atual: R$ 15.200</div>
+          <div className="card">📈 Rentabilidade: +12%</div>
+          <div className="card">📊 Investimentos: 8 ativos</div>
+          <div className="card">🎯 Meta: R$ 5.000 (80%)</div>
+        </div>  
+    </div>    
+        
+    
   )
 }
 
